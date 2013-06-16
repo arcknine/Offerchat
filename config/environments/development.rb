0b4403357df80c8c6e18a20e4a2a14cd3d956f54
Vincent Paca <vincent.paca@gmail.com>
2013-06-17 00:01:46 +0800

    System adds jabber user and password to new users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Dashboard::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Openfire Config
+  CHAT_SERVER_URL = "http://local.offerchat.com:9090/"
+  CHAT_SERVER_SECRET = "0ff3rch@t"
+  USER_SERVICE_ENDPOINT = "plugins/userService/userservice?"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = Fabricate(:user)
+  end
+
+  describe "when creating a new user" do
+    it "should have a jabber_user and jabber_password ready" do
+      @user.jabber_user.should_not eq(nil)
+      @user.jabber_password.should_not eq(nil)
+    end
+
+    it "should process an API request to Openfire to sidekiq" do
+      expect {
+        Fabricate(:user)
+      }.to change(JabberUserWorker.jobs, :size).by(1)
+    end
+  end
 end
