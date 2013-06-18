105c271cacb762bee8677d334354bd44d0118c78
Ian Bert Tusil <iyanski@gmail.com>
2013-06-18 17:23:57 +0800

    User should have an avatar spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe User do
+  
+  before(:each) do
+    @user = Fabricate(:user)
+  end
+  
   it { should have_many :accounts }
   it { should have_many :websites }
+  
+  it "should have an avatar" do
+    
+  end
 end
