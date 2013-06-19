35acce6c45693cef533af73878484b569c5a27d5
Keenan Iban <arcknine@skolpad.org>
2013-06-19 13:33:20 +0800

    Added comments

--- spec/lib/openfire_api_spec.rb
+++ spec/lib/openfire_api_spec.rb
@@ -14,7 +14,10 @@ describe OpenfireApi do
     # create 2nd account to be added as roster to 1st jabber account
     OpenfireApi.create_user(@user2.jabber_user, @user2.jabber_password).should eq(true)
 
+    # user is subscribed to user2
     OpenfireApi.subcribe_roster(@user2.jabber_user, @user.jabber_user, "test name", "test group").should eq(true)
+
+    # user2 is subscribed to user
     OpenfireApi.subcribe_roster(@user.jabber_user, @user2.jabber_user, "test name", "test group").should eq(true)
   end
 end
