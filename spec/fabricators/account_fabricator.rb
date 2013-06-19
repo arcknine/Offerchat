39760c4e473b2d662f08a9262a54accd3e278f70
Keenan Iban <arcknine@skolpad.org>
2013-06-19 18:39:13 +0800

    System adds rosters to agents/admin for the website

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -1,3 +1,15 @@
 Fabricator(:account) do
-  role        1
+  role 1
+end
+
+Fabricator(:owner, :class_name => :account) do
+  role 1
+end
+
+Fabricator(:admin, :class_name => :account) do
+  role 2
+end
+
+Fabricator(:agent, :class_name => :account) do
+  role 3
 end
