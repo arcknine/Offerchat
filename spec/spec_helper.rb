96c28ebf49b85de04f8589374394ab8f0cc851d5
Ian Bert Tusil <iyanski@gmail.com>
2013-06-19 10:20:27 +0000

    spec_helper.rb edited online with Bitbucket

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'sidekiq/testing'
-require "mocha/setup"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
