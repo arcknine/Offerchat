bdf52d585122d3fd6e8228d6286fba2b006a1ce6
Vincent Paul Paca <vincent.paca@gmail.com>
2013-06-20 02:39:53 +0000

    Updated tests

--- spec/lib/openfire_api_spec.rb
+++ spec/lib/openfire_api_spec.rb
@@ -6,18 +6,11 @@ describe OpenfireApi do
     @user2 = Fabricate(:user)
   end
 
-  it "should generate jabber account" do
+  it "should return true on create user" do
     OpenfireApi.create_user(@user.jabber_user, @user.jabber_password).should eq(true)
   end
 
-  it "should add roster" do
-    # create 2nd account to be added as roster to 1st jabber account
-    # OpenfireApi.create_user(@user2.jabber_user, @user2.jabber_password).should eq(true)
-
-    # user is subscribed to user2
+  it "should return true on subscribe roster" do
     OpenfireApi.subcribe_roster(@user2.jabber_user, @user.jabber_user, "test name", "test group").should eq(true)
-
-    # user2 is subscribed to user
-    # OpenfireApi.subcribe_roster(@user.jabber_user, @user2.jabber_user, "test name", "test group").should eq(true)
   end
 end
