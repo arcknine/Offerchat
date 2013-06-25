377e62c44b9bf3ecf0fddb3ea355ccc386f03302
Vincent Paul Paca <vincent.paca@gmail.com>
2013-06-25 02:28:51 +0000

    Updated method name to be more descriptive.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def has_website?
+  def current_user_has_website?
     head :unauthorized if current_user.websites.empty?
   end
 end
