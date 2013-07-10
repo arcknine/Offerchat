c103071a6331eac21ae2042b2dba50840e876793
Vincent Paca <vincent.paca@gmail.com>
2013-07-10 16:07:40 +0800

    Use staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :bundle_cmd, "/home/ubuntu/.rvm/gems/ruby-1.9.3-p429@global/bin/bundle"
 
 set :user, "ubuntu"
 set :use_sudo, false
-set :branch, "deploy"
+set :branch, "staging"
 set :rails_env, "staging"
 
 server "184.73.50.116", :app, :web, :db, :primary => true
