aff85b64abe425076077264378881202ab74bb1e
Vincent Paca <vincent.paca@gmail.com>
2013-07-10 15:18:45 +0800

    Added capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+require 'bundler/capistrano'
+require 'capistrano_colors'
+require 'rvm/capistrano'
+require 'capistrano/ext/multistage'
+require 'sidekiq/capistrano'
+
+# Basic
+set :stages, %w(staging production)
+set :application, "Offerchat"
+set :repository,  "set your repository location here"
+set :keep_releases, 5
+
+# SSH Options
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = ["~/.ssh/offerchat.pem"]
+
+# Deploy Config
+set :deploy_to, "/home/ubuntu/apps/offerchat"
+set :deploy_via, :remote_cache
+
+set :scm, :git
+set :repository, "git@bitbucket.org:offerchat/dashboard.git"
+
+# Run migrations
+before 'deploy:setup_db'
+after  'deploy:update_code', 'deploy:migrate'
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  after 'deploy:create_symlink', 'deploy:pictures:symlink'
+
+  namespace :pictures do
+    desc "Link images from shared to common"
+    task :symlink do
+      run "cd #{current_path}/public; rm -rf system; ln -s #{shared_path}/system ."
+    end
+  end
+
+  task :setup_db do
+    run "cp #{shared_path}/../../shared/database.yml.deploy #{release_path}/config/database.yml"
+  end
+
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+# Cleanup
+after 'deploy:restart', 'deploy:cleanup'
