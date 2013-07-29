15e022968fc61ac99146054ab633939c2db190f4
Eralph Amodia <eralph@offerchat.com>
2013-07-28 19:14:01 -0700

    Added test for checking offline message length

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -10,7 +10,7 @@ describe Website do
 
   describe "website creation" do
     before(:each) do
-      @website = Fabricate(:website)
+      @website = Fabricate(:website, :owner => Fabricate(:user))
     end
 
     it "should have url" do
@@ -60,7 +60,7 @@ describe Website do
 
   describe "after create" do
     before(:each) do
-      @website = Fabricate(:website)
+      @website = Fabricate(:website, :owner => Fabricate(:user))
     end
 
     it "should generate API key" do
@@ -74,7 +74,7 @@ describe Website do
 
     it "should generate 1 job to create 30 rosters for website owner" do
       expect {
-        Fabricate(:website)
+        Fabricate(:website, :owner => Fabricate(:user))
       }.to change(GenerateRostersWorker.jobs, :size).by(1)
     end
   end
@@ -106,14 +106,14 @@ describe Website do
 
   describe "when running methods from sidekiq" do
     it "should generate correct visitor id for website" do
-      @website = Fabricate(:website)
+      @website = Fabricate(:website, :owner => Fabricate(:user))
       @website.generate_visitor_id.should =~ /^visitor_[0-9]*_\d{6}/
     end
   end
 
   describe "after delete of website" do
     before(:each) do
-      @website = Fabricate(:website)
+      @website = Fabricate(:website, :owner => Fabricate(:user))
       Fabricate(:account, :website => @website)
     end
 
@@ -125,8 +125,10 @@ describe Website do
 
   describe "when updating website settings" do
     before(:each) do
-      @website = Fabricate(:website)
+      @website = Fabricate(:website, :owner => Fabricate(:user))
     end
+
+
 
     it "should update settings based on the parameters" do
       settings = {"style"=>{"theme"=>"cadmiumreddeep", "position"=>"right", "rounded"=>false, "gradient"=>true},
@@ -140,6 +142,20 @@ describe Website do
 
       @website.settings(:style).theme.should eq("cadmiumreddeep")
       @website.settings(:style).gradient.should be_true
+    end
+
+    describe "offline" do
+      it "description should be less than 140 characters" do
+        settings = {"style"=>{"theme"=>"cadmiumreddeep", "position"=>"right", "rounded"=>false, "gradient"=>true},
+          "online"=>{"header"=>"Chat with us", "agent_label"=>"Got a question? We can help.", "greeting"=>"Hi, I am",
+          "placeholder"=>"Type your message and hit enter"}, "pre_chat"=>{"enabled"=>false, "message_required"=>false,
+          "header"=>"Let me get to know you!", "description"=>"Fill out the form to start the chat."}, "post_chat"=>{"enabled"=>true,
+          "header"=>"Chat with me, I'm here to help", "description"=>"Please take a moment to rate this chat session"},
+          "offline"=>{"enabled"=>true, "header"=>"Contact Us", "description"=>"Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP.Leave a message and we will get back to you ASAP."}}
+        @website.save_settings(settings)
+
+        @website.errors.messages.should_not be_blank
+      end
     end
   end
 end
