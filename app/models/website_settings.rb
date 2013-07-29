2945675d54aa7fc35c29efe59cf4f5862dc34216
Eralph Amodia <eralph@offerchat.com>
2013-07-28 22:23:59 -0700

    Pre chat message should not be blank

--- app/models/website_settings.rb
+++ app/models/website_settings.rb
@@ -2,8 +2,15 @@ class WebsiteSettings < RailsSettings::SettingObject
   validate do
     case self.var
       # when "style"
-      # when "onine"
-      # when "pre_chat"
+      # when "online"
+
+      when "pre_chat"
+        if self.description.blank?
+          errors.add(:description, " should not be blank")
+        elsif self.description.length > 140
+          errors.add(:description, " should not exceed 140 characters")
+        end
+
       # when "post_chat"
 
       when "offline"
