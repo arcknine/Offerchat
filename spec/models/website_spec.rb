11603c2069f1694893c2ceca7f444b89750060c5
Eralph Amodia <eralph@offerchat.com>
2013-07-28 20:05:09 -0700

    Offline settings email should be valid

--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -206,6 +206,18 @@ describe Website do
         @website.save_settings(settings)
         @website.errors.messages.should_not be_blank
       end
+
+      it "email should be valid format" do
+        settings = {"style"=>{"theme"=>"cadmiumreddeep", "position"=>"right", "rounded"=>false, "gradient"=>true},
+        "online"=>{"header"=>"Chat with us", "agent_label"=>"Got a question? We can help.", "greeting"=>"Hi, I am",
+        "placeholder"=>"Type your message and hit enter"}, "pre_chat"=>{"enabled"=>false, "message_required"=>false,
+        "header"=>"Let me get to know you!", "description"=>"Fill out the form to start the chat."}, "post_chat"=>{"enabled"=>true,
+        "header"=>"Chat with me, I'm here to help", "description"=>"Please take a moment to rate this chat session"},
+        "offline"=>{"enabled"=>true, "header"=>"Contact Us", "description"=>"Leave a message and we will get back to you ASAP.", "email" => "erapguapo"}}
+        @website.save_settings(settings)
+
+        @website.errors.messages.should_not be_blank
+      end
     end
   end
 end
