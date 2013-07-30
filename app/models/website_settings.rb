21000d1190d7f6f46066788c40b0fbea000706c1
Eralph Amodia <eralph@offerchat.com>
2013-07-29 22:56:22 -0700

    Do not check for style and online settings

--- app/models/website_settings.rb
+++ app/models/website_settings.rb
@@ -1,17 +1,19 @@
 class WebsiteSettings < RailsSettings::SettingObject
   validate do
 
-    if self.description.blank?
-      errors.add(:description, " should not be blank")
-    elsif self.description.length > 140
-      errors.add(:description, " should not exceed 140 characters")
-    end
+    unless self.var == 'style' || self.var == 'online'
+      if self.description.blank?
+        errors.add(:description, " should not be blank")
+      elsif self.description.length > 140
+        errors.add(:description, " should not exceed 140 characters")
+      end
 
-    if self.var == 'offline' || self.var == 'post_chat'
-      if self.email.blank?
-        errors.add(:email, " should not be blank")
-      elsif !valid_email?(self.email)
-        errors.add(:email, " is invalid")
+      if self.var == 'offline' || self.var == 'post_chat'
+        if self.email.blank?
+          errors.add(:email, " should not be blank")
+        elsif !valid_email?(self.email)
+          errors.add(:email, " is invalid")
+        end
       end
     end
 
