bf5610e12fef6e56d71cdcc09f1425d035062f0c
Eralph Amodia <eralph@offerchat.com>
2013-07-29 20:01:59 -0700

    Post chat message should not be blank and less than 140 characters

--- app/models/website_settings.rb
+++ app/models/website_settings.rb
@@ -1,31 +1,20 @@
 class WebsiteSettings < RailsSettings::SettingObject
   validate do
-    case self.var
-      # when "style"
-      # when "online"
 
-      when "pre_chat"
-        if self.description.blank?
-          errors.add(:description, " should not be blank")
-        elsif self.description.length > 140
-          errors.add(:description, " should not exceed 140 characters")
-        end
-
-      # when "post_chat"
-
-      when "offline"
-        if self.description.blank?
-          errors.add(:description, " should not be blank")
-        elsif self.description.length > 140
-          errors.add(:description, " should not exceed 140 characters")
-        end
-
-        if self.email.blank?
-          errors.add(:email, " should not be blank")
-        elsif !valid_email?(self.email)
-          errors.add(:email, " is invalid")
-        end
+    if self.description.blank?
+      errors.add(:description, " should not be blank")
+    elsif self.description.length > 140
+      errors.add(:description, " should not exceed 140 characters")
     end
+
+    if self.var == 'offline' || self.var == 'post_chat'
+      if self.email.blank?
+        errors.add(:email, " should not be blank")
+      elsif !valid_email?(self.email)
+        errors.add(:email, " is invalid")
+      end
+    end
+
   end
 
   def valid_email?(email)
