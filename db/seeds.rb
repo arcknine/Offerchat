530fda0b404fe2242a2789278debb0cd20671d63
Vincent Paca <vincent.paca@gmail.com>
2013-07-30 11:14:40 +0800

    Added plan model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Pricing Plans
+Plan.delete_all
+Plan.create(:name => "Free", :description => "Forever Free", :price => 0, :max_agent_seats => 1)
+Plan.create(:name => "Starter", :description => "1 agent seat, unbrandable", :price => 14, :max_agent_seats => 1)
+Plan.create(:name => "Lite", :description => "2 to 5 agents, unbrandable", :price => 29, :max_agent_seats => 5)
+Plan.create(:name => "Business", :description => "6 to 12 agents, unbrandable", :price => 59, :max_agent_seats => 12)
+Plan.create(:name => "Pro", :description => "up to 30 agent seats, unbrandable", :price => 129, :max_agent_seats => 30)
---
+++ spec/fabricators/plan_fabricator.rb
@@ -0,0 +1,6 @@
+Fabricator(:plan) do
+  name            "MyString"
+  description     "MyString"
+  price           1.5
+  max_agent_seats 1
+end
