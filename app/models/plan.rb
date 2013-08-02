dbac4882b98cf5c516d482e9f54a55eb752805b3
Vincent Paca <vincent.paca@gmail.com>
2013-08-02 13:49:37 +0800

    Added subscription level fields to the user

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,8 +1,6 @@
 class Plan < ActiveRecord::Base
   self.primary_key = "plan_identifier"
-
   attr_accessible :description, :max_agent_seats, :name, :price, :features, :plan_identifier
   serialize :features, Array
-
   has_many :users, :foreign_key => "plan_identifier"
 end
