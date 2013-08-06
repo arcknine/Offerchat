a8989e6f89d65abd02b02c4eda8bd3ff2dcd63bf
Rommel Esparcia <wiredots01@ahoo.com.ph>
2013-08-06 15:52:34 +0800

    added widget form assets

--- app/mailers/widget_mailer.rb
+++ app/mailers/widget_mailer.rb
@@ -1,4 +1,5 @@
 class WidgetMailer < ActionMailer::Base
+  default from: "from@example.com"
 
   def offline_form(to, name, from, message)
     @data = {:full_name => name, :from => from, :message => message }
