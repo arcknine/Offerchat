d4db32be1bd5e26aeec332263873fec551e00ca9
Vincent Paca <vincent.paca@gmail.com>
2013-08-17 08:14:07 +0800

    Added tests

--- spec/services/unassign_agents_service_spec.rb
+++ spec/services/unassign_agents_service_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
 describe UnassignAgentsService do
-  xit "should retain " do
-    @owner   = Fabricate(:personal_user)
-    @website = Fabricate(:website, :owner => @owner)
-    @agent1  = Fabricate(:user)
-    @agent2  = Fabricate(:user)
-    @agent3  = Fabricate(:user)
+  it "should remove and retain agents" do
+    owner   = Fabricate(:personal_user)
+    website = Fabricate(:website, :owner => owner)
+    agent1  = Fabricate(:user)
+    agent2  = Fabricate(:user)
+    agent3  = Fabricate(:user)
 
-    Fabricate(:agent, :owner => @owner, :user => @agent1, :website => @website)
-    Fabricate(:agent, :owner => @owner, :user => @agent2, :website => @website)
-    Fabricate(:agent, :owner => @owner, :user => @agent3, :website => @website)
+    Fabricate(:agent, :owner => owner, :user => agent1, :website => website)
+    Fabricate(:agent, :owner => owner, :user => agent2, :website => website)
+    Fabricate(:agent, :owner => owner, :user => agent3, :website => website)
 
-    puts @owner.my_agents.count
-    agents = JSON.parse "{ \"0\": [#{@agent1.id}], \"1\": [#{@agent2.id}, true], \"2\": [#{@agent3.id}, false] }"
-    list = UnassignAgentsService.new(agents, @owner)
+    agents = JSON.parse "{ \"0\": [#{agent1.id}, false], \"1\": [#{agent2.id}, true], \"2\": [#{agent3.id}, false] }"
+
+    list = UnassignAgentsService.new(agents, owner)
     list.unassign
-    puts @owner.my_agents.count
+    owner.my_agents.count.should eq(1)
+    owner.my_agents.should_not include(agent1)
+    owner.my_agents.should_not include(agent3)
   end
 end
