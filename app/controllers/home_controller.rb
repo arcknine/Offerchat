974c0dad2fc749620fab2f5a2bc04728c9737775
Keenan Iban <arcknine@skolpad.org>
2013-08-19 10:59:38 +0800

    Modified config for staging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @user         = current_user
-    gon.chat_info = { bosh_url: CHAT_BOSH_URL, server_name: CHAT_SERVER_NAME }
+    gon.chat_info = { :bosh_url => ENV["CHAT_BOSH_URL"], :server_name => ENV["CHAT_SERVER_NAME"] }
     gon.current_user = current_user
     # gon.rabl "app/views/users/show.rabl", as: "current_user"
   end
