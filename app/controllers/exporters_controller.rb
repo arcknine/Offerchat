1d05e8b59f62d9568dbdcb14ecd5ee8cf5663e17
Rommel Esparcia <wiredots01@ahoo.com.ph>
2013-08-21 09:54:11 +0800

    initial code for migration new dash endpoint

--- app/controllers/exporters_controller.rb
+++ app/controllers/exporters_controller.rb
@@ -1,15 +1,20 @@
 class ExportersController < ApplicationController
   def index
-    if params[:data].present?
-      jsonArray = params[:data]
-      objArray = JSON.parse(jsonArray)
-      @content = ''
-      objArray.each do |msg|
-        @content += "(#{msg['time']}) #{msg['sender']}: #{msg['message']} \r\n"
-      end
-    end
-    send_data @content,
-      :type => 'text',
-      :disposition => "attachment; filename=offerchat_chat_logs.txt"
+    # if params[:data].present?
+    #   jsonArray = params[:data]
+    #   objArray = JSON.parse(jsonArray)
+    #   @content = ''
+    #   objArray.each do |msg|
+    #     @content += "(#{msg['time']}) #{msg['sender']}: #{msg['message']} \r\n"
+    #   end
+    # end
+    # send_data @content,
+    #   :type => 'text',
+    #   :disposition => "attachment; filename=offerchat_chat_logs.txt"
+
+    user = User.new
+    user.password ='123123123'
+    user.email ='xtestsdf@testasdfasdf.com'
+    user.name = 'rommel esparcia'
   end
 end
