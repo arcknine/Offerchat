24e4e00e0b851e7c265238a7f850fbb2a384d8be
Vincent Paca <vincent.paca@gmail.com>
2013-08-21 10:27:52 +0800

    Fix db seed file for admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ Plan.create(:name => "Business", :description => "for e-commerce websites", :pri
 Plan.create(:name => "Enterprise", :description => "for large sales companies", :price => 197, :max_agent_seats => 30, :features => ['up to 30 agent seats', 'white labeled widget'], :plan_identifier => "ENTERPRISE")
 
 # Admin User
-Admin.first.destroy
-Admin.create(:email => "vincent@offerchat.com", :password => "passw0rd", :password_confirmation => "passw0rd")
+Admin.where(:email => "admin@offerchat.com").delete_all
+Admin.create(:email => "admin@offerchat.com", :password => "passw0rd", :password_confirmation => "passw0rd")
