72179941c16295ce1d1c01cbae211d9f12b5548f
Ian Bert Tusil <iyanski@gmail.com>
2013-08-29 03:26:36 +0800

    Danica: Cleanup tab spaces for visitor fabricator

--- spec/fabricators/visitor_fabricator.rb
+++ spec/fabricators/visitor_fabricator.rb
@@ -1,10 +1,10 @@
 Fabricator(:visitor) do
-  website_id    		1
-  location    			"Makati, Philippines"
-  ipaddress    			"127.0.0.1"
-  browser    				"Google Chrome"
-  email    					""
-  name    					"visitor-026285"
-  country_code    	"PH"
+  website_id        1
+  location          "Makati, Philippines"
+  ipaddress         "127.0.0.1"
+  browser           "Google Chrome"
+  email             ""
+  name              "visitor-026285"
+  country_code      "PH"
   operating_system  "Mac"
 end
