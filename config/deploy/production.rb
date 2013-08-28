cce4fe2d0471115ee6a43443df2b27eb2a0d4576
Vincent Paca <vincent.paca@gmail.com>
2013-08-28 10:59:24 +0800

    Added deploy config for production

---
+++ config/deploy/production.rb
@@ -0,0 +1,13 @@
+ssh_options[:keys] = ["~/.ssh/offerchat.pem"]
+
+set :rvm_ruby_string, '1.9.3@offerchat'
+set :bundle_cmd, "/home/ubuntu/.rvm/gems/ruby-1.9.3-p429@global/bin/bundle"
+
+set :user, "ubuntu"
+set :use_sudo, false
+set :branch, "master"
+set :rails_env, "production"
+
+role :app, "54.211.184.139", "54.211.183.89", "54.211.187.204", "54.211.127.191"
+role :web, "54.211.184.139", "54.211.183.89", "54.211.187.204", "54.211.127.191"
+role :db, "54.211.184.139", :primary => true
