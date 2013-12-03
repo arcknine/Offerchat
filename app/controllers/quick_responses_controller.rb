3945f70e3e17cdf0fd69615222669859e02849db
Eralph Amodia <eralph@offerchat.com>
2013-12-02 18:17:53 -0800

    Added tests for quick responses

--- app/controllers/quick_responses_controller.rb
+++ app/controllers/quick_responses_controller.rb
@@ -7,18 +7,30 @@ class QuickResponsesController < ApplicationController
   end
 
   def create
-    @quick_response = QuickResponse.new(params[:quick_response])
-    @quick_response.user = current_user
+    if params[:quick_response][:shortcut].blank? or params[:quick_response][:message].blank?
+      render json: { errors: {"message" => ["Some fields are blank"]} }, status: 401
+    elsif params[:quick_response][:shortcut][0] != "/"
+      render json: { errors: {"message" => ["Shortcut invalid format"]} }, status: 401
+    else
+      @quick_response = QuickResponse.new(params[:quick_response])
+      @quick_response.user = current_user
 
-    unless @quick_response.save
-      respond_with @quick_response
+      unless @quick_response.save
+        respond_with @quick_response
+      end
     end
   end
 
   def update
-    @qr = QuickResponse.find_by_id params[:id]
-    unless @qr.update_attributes(params[:quick_response])
-      respond_with @qr
+    if params[:quick_response][:shortcut].blank? or params[:quick_response][:message].blank?
+      render json: { errors: {"message" => ["Some fields are blank"]} }, status: 401
+    elsif params[:quick_response][:shortcut][0] != "/"
+      render json: { errors: {"message" => ["Shortcut invalid format"]} }, status: 401
+    else
+      @qr = QuickResponse.find_by_id params[:id]
+      unless @qr.update_attributes(params[:quick_response])
+        respond_with @qr
+      end
     end
   end
 
