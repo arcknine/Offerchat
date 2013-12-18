ba314d88c37d22fd26bbecd67d4582860c8afdd0
Vincent Paca <vincent.paca@gmail.com>
2013-12-18 11:53:55 +0800

    Update new to app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,11 @@ Dashboard::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.asset_host = 'https://new.offerchat.com'
+  config.action_mailer.asset_host = 'https://app.offerchat.com'
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'new.offerchat.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'app.offerchat.com', :protocol => 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
