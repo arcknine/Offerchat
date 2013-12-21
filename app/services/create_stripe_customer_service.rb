578ac6fc1590bc1cd4910fe16e038d675f3404e4
Eralph Amodia <eralph@offerchat.com>
2013-12-22 06:22:35 +0800

    Bundle of changes

--- app/services/create_stripe_customer_service.rb
+++ app/services/create_stripe_customer_service.rb
@@ -1,10 +1,10 @@
 class CreateStripeCustomerService
-  def initialize(user, plan, card_token, coupon)
-    @user, @plan, @card, @coupon = user, plan, card_token, coupon
+  def initialize(user, plan, card_token, coupon, qty)
+    @user, @plan, @card, @coupon, @qty = user, plan, card_token, coupon, qty
   end
 
   def create
-    customer = Stripe::Customer.create :description => @user.name, :email => @user.email, :plan => @plan, :card => @card, :coupon => @coupon
+    customer = Stripe::Customer.create :description => @user.name, :email => @user.email, :plan => @plan, :card => @card, :coupon => @coupon, :quantity => @qty
     @user.update_attributes(:stripe_customer_token => customer.id, :plan_identifier => @plan)
   rescue => errors
     puts errors.inspect
@@ -13,7 +13,7 @@ class CreateStripeCustomerService
 
   def upgrade
     stripe = Stripe::Customer.retrieve @user.stripe_customer_token
-    stripe.update_subscription :plan => @plan, :prorate => true, :coupon => @coupon
+    stripe.update_subscription :plan => @plan, :prorate => true, :coupon => @coupon, :quantity => @qty
     @user.update_attribute(:plan_identifier, @plan)
   end
 end
